9ce0ec7092185a1376c81d47e9988f993120abc7
Hewr Srood <hewr_s@outlook.com>
2022-07-23 04:19:50 +0300

    configure gatsby

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -1,4 +1,9 @@
-import type { GatsbyConfig } from "gatsby"
+import type { GatsbyConfig } from "gatsby";
+
+require("dotenv").config({
+  path: `.env.${process.env.NODE_ENV}`,
+});
+const path = require(`path`);
 
 const config: GatsbyConfig = {
   siteMetadata: {
@@ -9,7 +14,34 @@ const config: GatsbyConfig = {
   // If you use VSCode you can also use the GraphQL plugin
   // Learn more at: https://gatsby.dev/graphql-typegen
   graphqlTypegen: true,
-  plugins: [],
-}
 
-export default config
+  plugins: [
+    `gatsby-plugin-layout`,
+    {
+      resolve: `gatsby-source-contentful`,
+      options: {
+        accessToken: process.env.CONTENTFUL_ACCESS_TOKEN,
+        spaceId: process.env.CONTENTFUL_SPACE_ID,
+      },
+    },
+    {
+      resolve: `gatsby-plugin-lodash`,
+      options: {
+        disabledFeatures: [`shorthands`, `cloning`],
+      },
+    },
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `images`,
+        path: path.join(__dirname, `src`, `images`),
+      },
+    },
+
+    `gatsby-transformer-sharp`,
+    `gatsby-plugin-sharp`,
+    `gatsby-plugin-image`,
+  ],
+};
+
+export default config;
