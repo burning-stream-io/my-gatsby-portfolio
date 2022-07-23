e3797ba3d97adabaa076dcc10f3e763d336c4633
Hewr Srood <hewr_s@outlook.com>
2022-07-23 04:53:48 +0300

    home page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,15 +1,49 @@
-import React from "react";
-import Banners from "../components/Banners";
-import Header from "../components/Header";
+import React, { FC } from "react";
+import { FullCard, HalfCard } from "../components/Cards";
+import Title from "../components/Title";
 import "../styles/index.css";
 
-function index() {
+const Home: FC = () => {
   return (
-    <div className="home">
-      <Header />
-      <Banners />
+    <div>
+      <Title title={"About me"} />
+      <FullCard
+        caption="Results-driven and motivated Software Engineer with demonstrated
+        experience in building modern beautiful software with optimized
+        performance, I enjoy building web/mobile applications, with interactive
+        design and learning new things in order to deliver better quality
+        products."
+      />
+      <Title title={"Tech stack"} />
+      <div className="half-cards-container">
+        <HalfCard
+          title="React Native"
+          caption="Build high performant pixel-perfect, buttery smooth UIs across both mobile platforms."
+          icon="https://cdn.worldvectorlogo.com/logos/react-native-1.svg"
+          rate={4.5}
+        />
+        <HalfCard
+          title="React"
+          caption="Build user interfaces with React, and let React handle the rendering of your data."
+          icon="https://cdn.worldvectorlogo.com/logos/react-2.svg"
+          rate={4}
+        />
+        <HalfCard
+          title="Node.js"
+          caption="Build scalable network applications with Node.js, Express, and Socket.io."
+          icon="https://cdn.worldvectorlogo.com/logos/nodejs.svg"
+          rate={3.5}
+        />
+
+        <HalfCard
+          title="NestJS"
+          caption="Build scalable network applications with Nest.js"
+          icon="https://cdn.worldvectorlogo.com/logos/nestjs.svg"
+          rate={3}
+        />
+      </div>
     </div>
   );
-}
+};
 
-export default index;
+export default Home;
