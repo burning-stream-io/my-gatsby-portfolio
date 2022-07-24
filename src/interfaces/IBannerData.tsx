5f65960f7edad2cb1dfe74df03472fff42515e9e
Hewr Srood <hewr_s@outlook.com>
2022-07-24 07:55:07 +0300

    get quotes from contentful

--- src/interfaces/IBannerData.tsx
+++ src/interfaces/IBannerData.tsx
@@ -1,10 +1,12 @@
 import IContentfulImage from "./IContentfulImage";
+import {RenderRichTextData} from "gatsby-source-contentful/rich-text";
 
 export default interface IBannerData {
   node: {
     title: string;
-    subtitle: string;
+    // subtitle: string;
     image: IContentfulImage[];
     icon: IContentfulImage;
+    quotes:RenderRichTextData<any> ;
   };
 }
