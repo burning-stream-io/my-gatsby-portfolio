6ef1cedd3fc3fcc07fc91577b4487fa183c22403
Hewr Srood <hewr_s@outlook.com>
2022-07-24 07:55:42 +0300

    get quotes from contentful

--- src/components/Banners/index.tsx
+++ src/components/Banners/index.tsx
@@ -1,82 +1,88 @@
-import { graphql, useStaticQuery } from "gatsby";
-import React, { useEffect, useRef, useState } from "react";
+import {graphql, useStaticQuery} from "gatsby";
+import React, {ReactNode, useEffect, useRef, useState} from "react";
 import IBannerData from "../../interfaces/IBannerData";
-import "./index.css";
-const Banners = () => {
-  const [activeBanner, setActiveBanner] = useState<number>(0);
+import {renderRichText} from 'gatsby-source-contentful/rich-text'
+import {BLOCKS} from '@contentful/rich-text-types'
 
-  const data = useStaticQuery(graphql`
-    {
-      allContentfulBanner {
-        edges {
-          node {
-            title
-            subtitle
-            icon {
-              file {
-                url
-                fileName
-                contentType
-              }
-            }
-            image {
-              file {
-                url
-                fileName
-                contentType
+import "./index.css";
+
+const Banners = () => {
+    const [activeBanner, setActiveBanner] = useState<number>(0);
+    const options = {
+        renderNode: {
+            [BLOCKS.UL_LIST]: (_: any, children: ReactNode) => {
+                return (
+                    <ul className="banner-list">
+                        {children}
+                    </ul>
+                )
+            },
+
+        },
+    }
+    const data = useStaticQuery(graphql`
+        {
+          allContentfulBanner {
+            edges {
+              node {
+                title
+                icon {
+                  file {
+                    url
+                    fileName
+                    contentType
+                  }
+                }
+                quotes {
+                  raw
+                }
+                image {
+                  file {
+                    url
+                    fileName
+                    contentType
+                  }
+                }
               }
             }
           }
         }
-      }
-    }
   `);
-  const banners: IBannerData[] = data.allContentfulBanner.edges;
-  const sliderRef = useRef<NodeJS.Timer>();
+    const banners: IBannerData[] = data.allContentfulBanner.edges;
+    const sliderRef = useRef<NodeJS.Timer>();
 
-  useEffect(() => {
-    sliderRef.current = setInterval(() => {
-      setActiveBanner((prevState) => (prevState + 1) % banners.length);
-    }, 5000);
+    useEffect(() => {
+        sliderRef.current = setInterval(() => {
+            setActiveBanner((prevState) => (prevState + 1) % banners.length);
+        }, 5000);
 
-    return () => {
-      clearInterval(sliderRef.current);
-    };
-  }, []);
-const quotes=[
-    `“Any fool can write code that a computer can understand. Good programmers write code that humans can understand.” – Martin Fowler`,
-    `“First, solve the problem. Then, write the code.” – John Johnson`,
-    `“Experience is the name everyone gives to their mistakes.” – Oscar Wilde`,
-    `In order to be irreplaceable, one must always be different” – Coco Chanel`,
-    `Learn continually. There is always "one more thing to learn" - Steve Jobs`
-]
-  return (
-    <div className="banner">
-      {banners.map((banner, i) => (
-        <>
-          <img
-            className={`banner-image ${activeBanner === i ? "active" : ""}`}
-            src={banner.node.image[0].file.url}
-            alt={`banner-${i}`}
-          />
-          <div
-            className={`banner-content ${activeBanner === i ? "active" : ""}`}
-          >
-            <h1 className="banner-title">{banners[i].node.title}</h1>
-            <ul className="banner-list">
-              {
-                quotes.map( (quote,i)=><li key={i}>{quote}</li>)
-              }
-            </ul>
-            <div className="banner-subtitle-container">
+        return () => {
+            clearInterval(sliderRef.current);
+        };
+    }, []);
 
-            </div>
-          </div>
-        </>
-      ))}
+    return (
+        <div className="banner">
+            {banners.map((banner, i) => (
+                <>
+                    <img
+                        className={`banner-image ${activeBanner === i ? "active" : ""}`}
+                        src={banner.node.image[0].file.url}
+                        alt={`banner-${i}`}
+                    />
+                    <div
+                        className={`banner-content ${activeBanner === i ? "active" : ""}`}
+                    >
+                        <h1 className="banner-title">{banners[i].node.title}</h1>
+                        {renderRichText(banner.node.quotes, options)}
+                        <div className="banner-subtitle-container">
+                        </div>
+                    </div>
+                </>
+            ))}
 
-    </div>
-  );
+        </div>
+    );
 };
 
 export default Banners;
