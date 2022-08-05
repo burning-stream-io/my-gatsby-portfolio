d2654e7c2be1f5b1d1f2de6df332b7eba52e955a
Hewr Srood <hewr_s@outlook.com>
2022-08-05 21:00:09 +0300

    format

--- src/interfaces/IBannerData.tsx
+++ src/interfaces/IBannerData.tsx
@@ -1,12 +1,11 @@
 import IContentfulImage from "./IContentfulImage";
-import {RenderRichTextData} from "gatsby-source-contentful/rich-text";
+import { RenderRichTextData } from "gatsby-source-contentful/rich-text";
 
 export default interface IBannerData {
   node: {
     title: string;
-    // subtitle: string;
     image: IContentfulImage[];
     icon: IContentfulImage;
-    quotes:RenderRichTextData<any> ;
+    quotes: RenderRichTextData<any>;
   };
 }
